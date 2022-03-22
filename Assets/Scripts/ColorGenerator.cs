7bf78a4abe8c6157ae89be0c823c53f6384d8f01
brandonvox <brandonvox9@gmail.com>
2022-03-22 20:44:02 +0700

    Delete Min value

--- Assets/Scripts/ColorGenerator.cs
+++ Assets/Scripts/ColorGenerator.cs
@@ -6,14 +6,13 @@ public class ColorGenerator
     int textureResolution; 
     ColorSettings colorSettings;
 
-
     public void HandleColorSettingsUpdated(ColorSettings colorSettings)
     {
         this.colorSettings = colorSettings;
         textureResolution = colorSettings.textureResolution; 
-        if(texture2D == null || texture2D.width != textureResolution * 2)
+        if(texture2D == null || texture2D.width != textureResolution)
         {
-            texture2D = new Texture2D(textureResolution * 2, 1, TextureFormat.ARGB32, false);
+            texture2D = new Texture2D(textureResolution, 1, TextureFormat.ARGB32, false);
         }
     }
 
@@ -26,17 +25,12 @@ public class ColorGenerator
     public void UpdateTexture2DPropertyInPlanetMaterial()
     {
         // HERE
-        Color[] colors = new Color[textureResolution * 2];  // Test with color 32 later
-        for(int i = 0; i < textureResolution * 2; i++)
-        {
-            if(i < textureResolution)
-            {
-                colors[i] = colorSettings.oceanGradient.Evaluate(i / (textureResolution - 1f));
-            }
-            else
-            {
-                colors[i] = colorSettings.planetGradient.Evaluate((i - textureResolution)/ (textureResolution - 1f));
-            }
+        Color[] colors = new Color[textureResolution];  // Test with color 32 later
+        for (int i = 0; i < textureResolution; i++)
+        {     
+                colors[i] = colorSettings.planetGradient
+                .Evaluate(i/ (textureResolution - 1f));
+            
 
         }
 
